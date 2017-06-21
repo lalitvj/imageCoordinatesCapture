c2f24f7582d2bc9fc0ea2caa7980d97830c81a23
Lalit Vijay <lalit.vijay952@gmail.com>
2017-06-22 00:36:53 +0530

    Sorting image by name

--- img.py
+++ img.py
@@ -9,7 +9,7 @@ import os
 
 def load_images_from_folder(folder):
     images = []
-    for filename in os.listdir(folder):
+    for filename in sorted(os.listdir(folder)):
         img = cv2.imread(os.path.join(folder,filename))
         if img is not None:
             images.append(img)
