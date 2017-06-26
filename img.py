6f3ce5c4fc3e81cdbaaa2e4d8a8d67756d7818a3
Lalit Vijay <lalit@ACA800B5.ipt.aol.com>
2017-06-26 19:08:46 +0530

    bit clean up

--- img.py
+++ img.py
@@ -1,26 +1,30 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from matplotlib.patches import Circle
 import os
 
 #----------------------------------
 # Loading all images from folder
 #----------------------------------
+imageFolder = './train_3_last'
+resultFile = "result.txt"
 images = []
 imageName = []
     
 def load_images_from_folder(folder):
     for filename in sorted(os.listdir(folder)):
-        img = cv2.imread(os.path.join(folder,filename))
-        if img is not None:
-            images.append(img)
-            imageName.append(filename)
-    return images
+        # img = cv2.imread(os.path.join(folder,filename))
+        # if img is not None:
+        #     images.append(img)
+        imageName.append(filename)
+        print filename
+    return imageName
 
 
-data_images = load_images_from_folder('./ilika');
+data_images = load_images_from_folder(imageFolder);
 print(data_images);
-
+print  len(data_images)
 # Global index value of image current postion.
 index = 0
 imagecordinate =[];    
@@ -32,25 +36,32 @@ imagecordinate =[];
 
 def onclick(event):
     if event.dblclick:
-    	if event.button == 1:
+        if event.button == 1:
             if len(imagecordinate) == 0:
-                imagecordinate.append(imageName[index]);    
+                imagecordinate.append(data_images[index]);    
             imagecordinate.append(event.xdata);
             imagecordinate.append(event.ydata);
+            #cv2.circle(img, (int(event.xdata),int(event.ydata)), 2, (255,255,0), 25)
+            #cv2.circle(img, (int(event.xdata),int(event.ydata)), 2, (255,0,0), 5)
+            #circ = Circle((int(event.xdata),int(event.ydata)),10)
+            #plt.add_patch(circ)
+            plt.scatter([10], [20])
+            plt.scatter(x=[30, 40], y=[50, 60], c='r', s=40)
+            plt.show()
             #write data for image cordinates
             if len(imagecordinate) == 5:
-                f = open("earPointData1.txt", "a");
-	    	    #f.write( 'x:'+str(event.xdata) + ',y:' + str(event.ydata) + "\n"  )
+                f = open(resultFile, "a");
+                #f.write( 'x:'+str(event.xdata) + ',y:' + str(event.ydata) + "\n"  )
                 f.write( str(imagecordinate) + "\n"  )
                 f.close();
                 imagecordinate[:] = [];
-	    	#showing the data on click event
-	    	print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-        	  (event.button, event.x, event.y, event.xdata, event.ydata));	
+            #showing the data on click event
+            print('button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+              (event.button, event.x, event.y, event.xdata, event.ydata));  
         
     else:
-		print(event)    		
-    	
+        print(event)            
+        
 cid = plt.connect('button_press_event', onclick)
 
 #----------------------------------
@@ -64,10 +75,14 @@ def on_key(event):
         global index
 
         index += 1
-
+        print index
         if index < len(data_images):
-            plt.imshow(data_images[index])
-            plt.draw()
+            img=[];
+            img = cv2.imread(os.path.join(imageFolder,data_images[index]))
+            if img is not None:
+                print img
+                plt.imshow(img)
+                plt.draw()
         else:        
             plt.close()
     
@@ -79,10 +94,16 @@ def on_key(event):
             plt.draw()
         else:        
             plt.close()
+            # index = len(data_images) -1
+            # print index
+            # plt.imshow(data_images[index])
+            # plt.draw()
     
 pid = plt.connect('key_press_event', on_key)
 
-plt.imshow(data_images[index])
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show();
+img = cv2.imread(os.path.join(imageFolder,data_images[index]))
+if img is not None:
+    plt.imshow(img)
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show();
 
